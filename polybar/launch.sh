43fe2532c2acdc40e6522447460646f49f4b26a3
danig <GodKayaki>
2021-01-24 18:51:15 +0100

    modified:   .bashrc 	modified:   .gitignore 	deleted:    alacritty/alacritty.yml 	deleted:    bashtop/bashtop.cfg 	deleted:    bashtop/user_themes/dracula.theme 	deleted:    cava/config 	deleted:    neofetch/config.conf 	deleted:    polybar/config 	deleted:    polybar/launch.sh 	deleted:    polybar/scripts/get_spotify_status.sh 	deleted:    polybar/scripts/scroll_spotify_status.sh 	deleted:    polybar/themes/miniblocks/config 	deleted:    polybar/themes/miniblocks/get_workspace 	deleted:    polybar/themes/miniblocks/launch.sh 	deleted:    polybar/themes/miniblocks/scrot.png 	deleted:    rofi/config.rasi 	deleted:    rofi/themes/denvit.rasi 	deleted:    spicetify/Themes/Purple-vibrant/color.ini 	deleted:    spicetify/Themes/Purple-vibrant/user.css 	deleted:    spicetify/config.ini 	deleted:    wallpapers/28092019-DSC_0548.jpg 	deleted:    wallpapers/background.png 	deleted:    wallpapers/blue-red-moon-blur.jpg 	deleted:    wallpapers/blue-red-moon.jpg 	deleted:    wallpapers/pixel-art-wallpaper-20072219595320.jpg 	deleted:    wallpapers/pixel-art-wallpaper-20072219595320.png 	deleted:    wallpapers/spacesurf.png 	deleted:    wallpapers/wallpapersden.com_-bit-sunset_1920x1080.jpg

--- polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar top | tee -a /tmp/polybar_top.log & disown
