ec95f5614a90e6d70feff9cb49687047fd7a0571
danig <GodKayaki>
2021-01-30 22:32:15 +0100

    polybar power menu

--- resolution.sh
+++ resolution.sh
@@ -2,3 +2,4 @@ xrandr --newmode "1920x1080_60.00" 173.00  1920 2048 2248 2576  1080 1083 1088 1
 xrandr --addmode eDP-1-1 1920x1080_60.00
 xrandr --output eDP-1-1 --mode 1920x1080_60.00
 nitrogen --restore
+bash ~/.config/polybar/launch.sh
